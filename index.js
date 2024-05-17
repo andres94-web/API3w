f49ff14d36c73ebbbfb4adcbade8b8bd5bd54c42
andres94-web <andresvallelap@gmail.com>
2024-05-17 05:55:31 -0500

    quizas funcione la url para consumir la api me lo acabo de soñar

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import cors from "cors"
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 const readData = () => {
   try {
